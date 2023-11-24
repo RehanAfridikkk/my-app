dd19f16935d407689a435e2540bb980f54606010
RehanAfridikkk <rehan.gull12345@gmail.com>
2023-11-24 11:47:02 +0500

    react UI assignment

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import Home from './components/Home/Home';
 import './App.css';
-
+import {Routes,Route} from 'react-router-dom'
+import Api from './components/API/Api';
+import Game from './components/Game/Tic-Tac';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className='App'>
+    <div>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/api" element={<Api/>}/>
+      <Route path="/game" element={<Game/>}/>
+
+    </Routes>
     </div>
+    </div>
+     
+     </>
   );
 }
 
